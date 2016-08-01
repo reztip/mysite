22d4539cd45e2818a8218e41503d0bbfbe80c9aa
reztip <alexpitzer42@gmail.com>
2016-07-31 18:51:14 -0700

    first commit

--- controller/views.py
+++ controller/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import django.views.generic as generic
-# Create your views here.
 
 class AboutView(generic.TemplateView):
     template_name = "controller/about.html"
+
+class ResumeView(generic.TemplateView):
+    template_name = "controller/resume.html"
+
+class BlogView(generic.ListView):
+    template_name = "controller/blog.html"
+
+class ProjectsView(generic.ListView):
+    template_name = "controller/projects.html"
+
+class OtherView(generic.ListView):
+    template_name = "controller/other.html"
+
+def home(request):
+    return HttpResponse("Lorem ipsum, this is home!")
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,14 @@ from django.contrib import admin
 from controller import views as controller_views
 
 urlpatterns = [
-    url(r'^about/', controller_views.AboutView.as_view()),
+    # Home page.
+    url(r'^$', controller_views.home, name = "home"),
+    # The aboutme page
+    url(r'^about/$', controller_views.AboutView.as_view(),
+        name = "about"),
+    url(r'^resume/$', controller_views.ResumeView.as_view()),
+    url(r'^blog/$', controller_views.BlogView.as_view()),
+    url(r'^projects/$', controller_views.ProjectsView.as_view()),
+    url(r'^other/$', controller_views.OtherView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
