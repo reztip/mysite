9a74425ed5f77754829c146b72ec44027cbcf9d8
reztip <alexpitzer42@gmail.com>
2016-07-31 22:28:10 -0700

    first failing pass at return a pdf resume

--- controller/views.py
+++ controller/views.py
@@ -5,6 +5,16 @@ import django.views.generic as generic
 class AboutView(generic.TemplateView):
     template_name = "controller/about.html"
 
+def resume_view(request):
+    with open('../static/controller/resume.pdf', 'rb') as pdf:
+        print(pdf)
+        response = HttpResponse(content = pdf)
+        response['Content-Type'] = 'application/pdf'
+        response['Content-Disposition'] = 'attachment; filename = resume.pdf'
+        return response
+
+
+
 class ResumeView(generic.TemplateView):
     template_name = "controller/resume.html"
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,13 +5,13 @@ from controller import views as controller_views
 
 urlpatterns = [
     # Home page.
-    url(r'^$', controller_views.home, name = "home"),
+    url(r'^$', controller_views.HomeView.as_view(), name = "home"),
     # The aboutme page
     url(r'^about/$', controller_views.AboutView.as_view(),
         name = "about"),
-    url(r'^resume/$', controller_views.ResumeView.as_view()),
-    url(r'^blog/$', controller_views.BlogView.as_view()),
-    url(r'^projects/$', controller_views.ProjectsView.as_view()),
-    url(r'^other/$', controller_views.OtherView.as_view()),
+    url(r'^resume/$', controller_views.resume_view, name = 'resume'),
+    url(r'^blog/$', controller_views.BlogView.as_view(), name = 'blog'),
+    url(r'^projects/$', controller_views.ProjectsView.as_view(), name = 'projects'),
+    url(r'^other/$', controller_views.OtherView.as_view(), name = 'other'),
     url(r'^admin/', admin.site.urls),
 ]
