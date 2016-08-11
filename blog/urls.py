c8a3c3926529c7aac943dcc8f136dd2f2b035f51
reztip <alexpitzer42@gmail.com>
2016-08-10 23:01:08 -0700

    add blog app

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from . import views as blog_views
+urlpatterns = [
+    url(r'^$', blog_views.index, name = 'index') 
+        ]
