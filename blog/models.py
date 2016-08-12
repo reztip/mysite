1cc798e43e9aeca5fad1b00b21e11f6f27002636
reztip <alexpitzer42@gmail.com>
2016-08-11 23:27:25 -0700

    fix Post to allow blank publish_date - migrate

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete = models.CASCADE )
     title = models.CharField(max_length = 70, blank = False)
     body = models.TextField(blank = False)
-    publish_date = models.DateField()
+    publish_date = models.DateField(blank = True)
     created_date = models.DateField(auto_now_add = True)
 
     def publish(self):
