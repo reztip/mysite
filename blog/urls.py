fce7fa7d1975be8acd83622ecd26cc03ac58de39
reztip <alexpitzer42@gmail.com>
2016-08-28 22:15:27 -0700

    add editable feature to blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views as blog_views
 urlpatterns = [
     url(r'^$', blog_views.index, name = 'index'),
     url(r'^(?P<pk>\d+)/$', blog_views.detail, name = 'detail'),
+    url(r'^(?P<pk>\d+)/edit/$', blog_views.edit, name = 'edit'),
     ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
 def index(request):
     posts = Post.objects.all()
-    paginator = Paginator(posts, 8)
-    return render(request, 'blog/index.html', context = {'posts': paginator.page(1)})
+    return render(request, 'blog/index.html', context = {'posts': posts})
 
 def detail(request, pk):
     post = Post.objects.get(pk = pk)
     return render(request, 'blog/detail.html', context = {'post': post})
+
+def edit(request, pk):
+    post = Post.objects.get(pk = pk)
+    if request.method == "POST":
+         new_text = request.POST.get('post_body', False)
+         if new_text:
+             post.body = new_text
+             post.save()
+             return redirect(reverse('blog:detail', args=(post.id,)))
+         else:
+            return render(request, 'blog/edit.html', context = {'post': post})
+    else:
+        return render(request, 'blog/edit.html', context = {'post': post})
+
