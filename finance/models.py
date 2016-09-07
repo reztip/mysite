9202d30869545bfb6e3b3f62b6537cfa02d96206
reztip <alexpitzer42@gmail.com>
2016-09-06 22:51:17 -0700

    basic model definitions --pre migration

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,54 @@
-from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import User
+from django.core.validators import MinValueValidator
+
+class Stock(models.Model):
+    ticker = models.CharField(max_length = 4, unique = True)
+    
+
+class History(models.Model):
+    pass
+
+class Transaction(models.Model):
+    date = models.DateTimeField(editable = False, auto_add_now = True)
+    stock = models.ForeignKeyField(Stock, blank = False, null = False)
+    quantity = models.PositiveIntegerField( blank = False, null = False)
+    history = models.ForeignKeyField(History, on_delete = models.CASCADE)
+    price = models.DecimalField(max_digits = 9, 
+            decimal_places = 2,
+            blank = False,
+            null = False,
+            validators = MinValueValidator(Decimal('0.01')),
+        )
+
+    @property
+    def total(self):
+        return self.price * self.quantity
+
+    class Meta:
+        abstract = True
+
+class SellTransaction(Transaction):
+    def __str__(self):
+        return "{0} units of {1} sold at {2}/share for a total of {3}".format \
+            (self.quantity, self.stock, self.price,
+            self.quantity * self.price
+            )
+
+
+class BuyTransaction(Transaction):
+    def __str__(self):
+        return "{0} units of {1} bought at {2}/share for a total of {3}".format \
+            (self.quantity, self.stock, self.price,
+            self.quantity * self.price
+            )
+
+class Account(models.Model):
+  user = models.OneToOneField(User, on_delete = models.CASCADE, editable = False)
+  balance = models.PositiveIntegerField()
+  # each account has a history of transactions, deposits
+  history = models.OneToOneField(History, on_delete = models.CASCADE)
+
+
